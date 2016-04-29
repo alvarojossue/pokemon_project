660326d112b4e19afeabd7d42c1c88a2e3c45d86
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-29 09:21:42 -0400

    Display image of the Pokemon

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -5,6 +5,18 @@ PokemonApp.Pokemon = function(pokemonUri){
 	this.id = PokemonApp.Pokemon.idFromUri(pokemonUri);
 };
 
+// Use of object or class
+
+$(document).on("ready", function(){
+	$(".js-show-pokemon").on("click", function(event){
+		var $button = $(event.currentTarget);
+		var pokemonUri = $button.data("pokemon-uri");
+
+		var pokemon = new PokemonApp.Pokemon(pokemonUri);
+		pokemon.render();
+	});
+});
+
 PokemonApp.Pokemon.prototype.render = function(){
 	console.log("Rendering pokemon: #" + this.id);
 
@@ -27,7 +39,32 @@ PokemonApp.Pokemon.prototype.render = function(){
 			$(".js-pkmn-sp-att").text(self.info.sp_atk);
 			$(".js-pkmn-sp-df").text(self.info.sp_def);
 			$(".js-pkmn-speed").text(self.info.speed);
+			displayTypes(self.info.types);
 
+			$.ajax({
+				url: getUri(self.info.sprites),
+				success: function(data){
+				console.log(data)
+				displayImage(data)},
+				error: function(error){
+					console.log("Failed")
+				}
+			})
+
+			$.ajax({
+				url: getUriDes(self.info.descriptions) ,
+				success: function(data){
+					console.log(data)
+					displayDescription(data)
+				},
+				error: function(error){
+					console.log("Failed!")
+				}
+			})
+
+
+			$(".js-image").empty();
+			$(".js-description").empty();
 			$(".js-pokemon-modal").modal("show");
 
 		},
@@ -43,15 +80,48 @@ PokemonApp.Pokemon.idFromUri = function(pokemonUri){
 	return uriSegments[secondLast];
 };
 
+// ---------- Display the type of the Pokemon -----------//
+function displayTypes(the_array){
+	the_array.forEach(function(the_item){
+		var the_type = `
+		${the_item.name}`
+		$(".js-pkmn-types").text(the_type);
+	})
+}
 
-// Use of object or class
+// ---------- Getting uri for sprites ------- //
 
-$(document).on("ready", function(){
-	$(".js-show-pokemon").on("click", function(event){
-		var $button = $(event.currentTarget);
-		var pokemonUri = $button.data("pokemon-uri");
+function getUri(the_array){
+	return the_array[0].resource_uri               
+}
 
-		var pokemon = new PokemonApp.Pokemon(pokemonUri);
-		pokemon.render();
-	});
-});
+// -------- Display Image ------- //
+
+function displayImage(the_object){
+	var html = `
+				<img src="http://pokeapi.co/${the_object.image}">`
+				$(".js-image").append(html);
+}
+
+// ------- Getting uri for description ------ //
+
+function getUriDes(the_array){
+	gen5 = []
+	the_array.forEach(function(the_item){
+		if (the_item.name.includes("6")){
+			gen5.push(the_item)
+		}
+	})
+	return gen5[0].resource_uri
+}
+
+//-------- Display Description -------//
+
+function displayDescription(the_object){
+	var html = `<p>${the_object.description}</p>`
+	$(".js-description").append(html)
+}
+
+
+
+
